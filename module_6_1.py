a73e5e2ab6c8d4f6f7c1e173a6d51bd6ba1c7256
nvstefan <nvstefan@mail.ru>
2024-10-05 11:04:12 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,52 @@
+# Домашнее задание
+# по теме "Зачем нужно наследование"
+
+# Задача "Съедобное, несъедобное":
+
+class Animal:
+    alive = True
+    fed = False
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        if food.edible == True:
+            Animal.fed = True
+            print(f'{self.name} съел {food.name}')
+        else:
+            Animal.alive = False
+            print(f'{self.name} не стал есть {food.name}')
+
+class Plant:
+    edible = False
+    def __init__(self, name):
+        self.name = name
+
+class Mammal(Animal):
+    pass
+
+class Predator(Animal):
+    pass
+
+class Flower(Plant):
+    pass
+
+class Fruit(Plant):
+    edible = True
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
+
